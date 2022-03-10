4aa2ed28906799bbfa34e6fac985038b260c46ff
rama krishna <krishnachowdary3991@gmail.comcd /home/ec2-user/cler11clearmkdir projectcode11 git config --global user.email krishnachowdary3991@gmail.com>
2022-03-10 08:27:23 +0530

    hello

---
+++ add.py
@@ -0,0 +1,10 @@
+#this program adds two numbers
+
+num1=[D [C = 1.5
+num2 = 6.3
+
+#add two numbers
+num=[D [= num1+[D [+ num2
+
+#Display the sum
+print('the sum of{0} and {1} is {2}'.format(num1, num2, sum))
